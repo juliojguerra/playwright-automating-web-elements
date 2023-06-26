6c3ea77f8a08706f322846500a08a3a2a21cdb31
Julio Guerra <j.guerratamayo@gmail.com>
2023-06-26 10:02:20 +0200

    Improve drag and drop test assertions

--- tests/dragAndDrop.spec.ts
+++ tests/dragAndDrop.spec.ts
@@ -1,13 +1,19 @@
 import { test, expect } from "@playwright/test";
 
-test.only("Drag and drop a box item", async function ({ page }) {
+test("Drag and drop a box item", async function ({ page }) {
   await page.goto("https://the-internet.herokuapp.com/drag_and_drop");
 
   const boxA = page.locator("#column-a");
   const boxB = page.locator("#column-b");
 
-  await boxA.dragTo(boxB);
+  const boxAContentBefore = await boxA.textContent();
+  const boxBContentBefore = await boxB.textContent();
 
-  await expect(boxA).toBeVisible();
-  await expect(boxB).toBeVisible();
+  await boxA.dragTo(boxA);
+
+  const boxAContentAfter = await boxA.textContent();
+  const boxBContentAfter = await boxB.textContent();
+
+  await expect(boxAContentBefore === boxBContentAfter).toBeTruthy();
+  await expect(boxBContentBefore === boxAContentAfter).toBeTruthy();
 });
